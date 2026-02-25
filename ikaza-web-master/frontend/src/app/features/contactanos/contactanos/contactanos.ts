3aad86de777afd0a3eee94ef0137961fb93f5733
Cristina Astocaza <cristina01astocaza@gmail.com>
2026-02-25 01:54:40 -0500

    Remove ContactForm from imports in ContactanosComponent

--- ikaza-web-master/frontend/src/app/features/contactanos/contactanos/contactanos.ts
+++ ikaza-web-master/frontend/src/app/features/contactanos/contactanos/contactanos.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ContactChannels } from '../contact-channels/contact-channels';
 import { LocationMap } from '../location-map/location-map';
-import { ContactForm } from '../contact-form/contact-form';
+
 
 @Component({
   selector: 'app-contactanos',
   standalone: true,
-  imports: [ContactChannels, LocationMap, ContactForm],
+  imports: [ContactChannels, LocationMap],
   templateUrl: './contactanos.html',
   styleUrl: './contactanos.scss'
 })
